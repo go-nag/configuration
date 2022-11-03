db0765fcb6e3bfdd8aab62aa7a5477da838a3aae
Pavle Prica <pavle.prica5@gmail.com>
2022-11-03 06:18:18 +0100

    Add env file loading

--- loader/env_file.go
+++ loader/env_file.go
@@ -1,1 +1,18 @@
 package loader
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+func LoadDefaultEnvFile() {
+	if err := godotenv.Load(); err != nil {
+		_ = fmt.Errorf("unable to load .env file. Error: %s", err)
+	}
+}
+
+func LoadEnvFile(envFilePath string) {
+	if err := godotenv.Load(envFilePath); err != nil {
+		_ = fmt.Errorf("unable to load .env file. Path: %s Error: %s", envFilePath, err)
+	}
+}
