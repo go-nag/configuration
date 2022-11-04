85c08c7fbcf00b4a729d500116fc18b7de25283f
Pavle Prica <pavle.prica5@gmail.com>
2022-11-04 22:12:17 +0100

    Add GetEnvOrDefault tests

--- configuration/environment_test.go
+++ configuration/environment_test.go
@@ -34,3 +34,30 @@ func TestGetEnv(t *testing.T) {
 		}
 	}
 }
+
+func TestGetEnvOrDefault(t *testing.T) {
+	loader.LoadEnvFile("../.env.example")
+	t.Log("When using GetEnvOrDefault")
+	{
+		t.Log("\tWhen variable present")
+		{
+			t.Log("\t\tShould return variable value")
+			{
+				variable := GetEnvOrDefault("TEST_STR", "DEFAULT_VALUE")
+
+				assert.Equal(t, "This is some cool str", variable)
+				assert.NotEqual(t, "DEFAULT_VALUE", variable)
+			}
+		}
+		t.Log("\tWhen variable not present")
+		{
+			t.Log("\t\tShould return default value")
+			{
+				variable := GetEnvOrDefault("TEST_STR_NOT_PRESENT", "DEFAULT_VALUE")
+
+				assert.NotEqual(t, "This is some cool str", variable)
+				assert.Equal(t, "DEFAULT_VALUE", variable)
+			}
+		}
+	}
+}
