dd759213ad779a2d290c1aa1084bbce07a768543
Pavle Prica <pavle.prica5@gmail.com>
2022-11-08 19:02:36 +0100

    Add TestManager_Get

--- cfg_m/manager_test.go
+++ cfg_m/manager_test.go
@@ -1,1 +1,42 @@
 package cfg_m
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestManager_Get(t *testing.T) {
+	var manager ConfigStringGetter
+	t.Log("\tWhen using Get")
+	{
+		t.Log("\t\tWhen value not present")
+		{
+			manager = &Manager{
+				make(map[string]string),
+			}
+			t.Logf("\t\t\tShould return %s error", configurationValueNotFoundMessage)
+			{
+				cfgValue, err := manager.Get("test.value")
+
+				assert.NotNil(t, err)
+				assert.Equal(t, "", cfgValue)
+				assert.True(t, strings.Contains(err.Error(), configurationValueNotFoundMessage))
+			}
+		}
+		t.Log("\t\tWhen value present")
+		{
+			testMap := make(map[string]string)
+			testMap["test.value"] = "some_value"
+			manager = &Manager{
+				testMap,
+			}
+			t.Log("\t\t\tShould return value")
+			{
+				cfgValue, err := manager.Get("test.value")
+				assert.Nil(t, err)
+				assert.Equal(t, "some_value", cfgValue)
+			}
+		}
+	}
+}
