74db9823ab791db993e40f46049637dbb65e8478
Pavle Prica <pavle.prica5@gmail.com>
2022-11-11 21:18:14 +0100

    Add unmarchalYamlContent

--- conf_loader/yaml_file.go
+++ conf_loader/yaml_file.go
@@ -14,3 +14,23 @@ var (
 func LoadConfigFile(environment string) (*cfg_m.Manager, error) {
 	return nil, errors.New("not implemented")
 }
+
+func unmarshalYamlContent(objectBase string, yamlContent interface{}, configuration map[string]string) {
+	v := reflect.ValueOf(yamlContent)
+
+	for _, key := range v.MapKeys() {
+		var base string
+		if len(objectBase) == 0 {
+			base = key.String()
+		} else {
+			base = fmt.Sprintf("%s.%s", objectBase, key)
+		}
+		value := v.MapIndex(key)
+
+		if value.Kind() == reflect.Map {
+			unmarshalYamlContent(base, value.Interface(), configuration)
+		} else {
+			configuration[base] = value.String()
+		}
+	}
+}
