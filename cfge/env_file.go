5da90ae96f35a261d806f5038917abb8bdabd6e6
Pavle Prica <pavle.prica5@gmail.com>
2022-11-29 21:12:38 +0100

    Refactor LoadEnvFile methods to log when error

--- cfge/env_file.go
+++ cfge/env_file.go
@@ -3,16 +3,17 @@ package cfge
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"log"
 )
 
 func LoadDefaultEnvFile() {
 	if err := godotenv.Load(); err != nil {
-		_ = fmt.Errorf("unable to load .env file. Error: %s", err)
+		log.Println(fmt.Errorf("unable to load .env file. Error: %s", err))
 	}
 }
 
 func LoadEnvFile(envFilePath string) {
 	if err := godotenv.Load(envFilePath); err != nil {
-		_ = fmt.Errorf("unable to load .env file. Path: %s Error: %s", envFilePath, err)
+		log.Println(fmt.Errorf("unable to load .env file. Path: %s Error: %s", envFilePath, err))
 	}
 }
