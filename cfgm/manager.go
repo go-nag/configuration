375afa9b8d77486c2c2d23aaf36c66bbc0faee6d
Pavle Prica <pavle.prica5@gmail.com>
2022-12-04 20:49:22 +0100

    Add GetArr interface
    Add GetArr interface

--- cfgm/manager.go
+++ cfgm/manager.go
@@ -11,9 +11,16 @@ var (
 
 // ConfigStringGetter is responsible for allowing getting of loaded configuration from the implemented types.
 type ConfigStringGetter interface {
-	// Get returns the configuration value that has been previously loaded/
+	// Get returns the configuration value that has been previously loaded.
 	// In event that the value isn't present it returns an error.
 	Get(configurationName string) (string, error)
+}
+
+// ConfigArrayGetter is responsible for parsing and getting config values that are represented as
+type ConfigArrayGetter interface {
+	// GetArr returns the configuration in an array form that has been previously loaded.
+	// In event that the value isn't present it returns an empty array.
+	GetArr(configurationName string) []string
 }
 
 // ConfigStringGetterWithDefault is responsible for allowing getting of loaded configuration from the implemented types.
@@ -22,6 +29,21 @@ type ConfigStringGetterWithDefault interface {
 	// GetOrDefault returns the configuration value that has been previously loaded/
 	// In event that the value isn't present it returns the provided default value.
 	GetOrDefault(configurationName string, defaultValue string) string
+}
+
+// Represents currently supported types by the configuration
+var (
+	str configType = 0
+	arr configType = 1
+)
+
+// configType represents the type of the configuration value
+// see configValue
+type configType = int
+
+type configValue struct {
+	value   string
+	cfgType configType
 }
 
 // Manager is used as a loaded configuration store.
